c8ccaf8d75f845b9c20856c9aaf6ace42a498b1d
Erik Fernando <erikfernandu@gmail.com>
2023-11-30 16:24:46 -0300

    Integração gráfica com tkinter

--- mongDao.py
+++ mongDao.py
@@ -4,84 +4,60 @@
 
 import os, tkMessageBox, pymongo, datetime, pickle, mongRecog
 import mongRecog as mrecog
-import numpy as np
 from pymongo import MongoClient
 
 conn = MongoClient('localhost', 27017)
 db = conn.facerecog_database
 peopcol = db.people_collection
 globcol = db.global_collection
-statusCam = False
-insertedPerson=None
 
-def startUp():
-    if int(globcol.count())==0:
-        globcol.insert({'labels_global': []})
-#        globcol.insert({'images_global': []})
-    else:
-        mrecog.recognizer.load('save.xml')
-        mrecog.imagens = pickle.load( open("imagens", "rb"))
+if os.path.isfile('save.xml')==True and os.path.isfile('images')==True:
+    mrecog.recognizer.load('save.xml')
+    lab_ = globcol.find()
+    for i in lab_:
+        mrecog.labels = i['labels_global']
+    mrecog.images = pickle.load( open("images", "rb"))
+else:
+    globcol.insert({ 'labels_global': []})
 
-def getInsertedPerson():
-    return insertedPerson
+def insertData(path, p_name, p_age):
+    try:
+	path_images= [os.path.join(path, f) for f in os.listdir(path)]
+	mrecog.trainImages(path_images, int(peopcol.count())+1)
+	for x in range(0, len(os.listdir(path))):
+	    globcol.update( {}, {'$push': { 'labels_global': int(peopcol.count())+1} } )
+	peopcol.insert( { "p_id": int(peopcol.count())+1, "p_name": p_name, "p_age": p_age, "p_qntimg": len(os.listdir(path)), "p_insertdate": datetime.datetime.utcnow()} )
+	return
+    except IOError:
+	tkMessageBox.showinfo("Aviso", "Diretorio Invalido")
 
-def setInsertedPerson(newUser):
-    insertedPerson=newUser
-
-def getStatusCam():
-    return statusCam
-
-def setStatusCam(on_off):
-    statusCam = on_off
-
-def getComboNames():
+def updateData(path, p_name):
+    try:
+	path_images= [os.path.join(path, f) for f in os.listdir(path)]
+	p_id = peopcol.find_one({'p_name': p_name})
+	mrecog.updateTrain(path_images, p_id['p_id'])
+	for x in range(0, len(os.listdir(path))):
+	     globcol.update( {}, {'$push': { 'labels_global': int(p_id['p_id'])} } )
+	peopcol.update_one({"p_id": p_id['p_id']}, {"$set": {"p_qntimg": p_id['p_qntimg']+len(os.listdir(path))}})
+	return
+    except IOError:
+	tkMessageBox.showinfo("Aviso", "Diretorio Invalido")
+    
+def getNames():
     combolist = []
     combo_ = peopcol.find()
     for i in combo_:
 	combolist.append(i['p_name'])
     return combolist
 
-def insertData(path, p_name, p_age):
-    try:
-	number = int(peopcol.count())+1
-	if getStatusCam():
-            path_images= path
-	else:
-            path_images= [os.path.join(path, f) for f in os.listdir(path)]
-        lab_ = globcol.find_one()
-        labels = lab_['labels_global']
-	returnQnt = mrecog.prepareImages(path_images, labels, number, getStatusCam())
-	for x in range(0, returnQnt):
-	    globcol.update( {}, {'$push': { 'labels_global': number} } )
-	peopcol.insert( { "p_id": number, "p_name": p_name, "p_age": p_age, "p_qntimg": returnQnt, "p_insertdate": datetime.datetime.utcnow()} )
-    except IOError:
-	tkMessageBox.showinfo("Aviso", "Diretorio Invalido")
-
-def updateData(path, p_name):
-    try:
-        p_id = peopcol.find_one({'p_name': p_name})
-	if getStatusCam():
-            path_images = path
-        else:
-            path_images= [os.path.join(path, f) for f in os.listdir(path)]
-	returnQnt = mrecog.updateTrain(path_images, p_id['p_id'], getStatusCam())
-        for x in range(0, returnQnt):
-            globcol.update( {}, {'$push': { 'labels_global': int(p_id['p_id'])} } )
-        peopcol.update_one({"p_id": p_id['p_id']}, {"$set": {"p_qntimg": p_id['p_qntimg']+returnQnt}})
-        return
-    except IOError:
-        tkMessageBox.showinfo("Aviso", "Diretorio Invalido")
-
 def recognition(findFace):
-    lab_ = globcol.find_one()
-    labels = lab_['labels_global']
-    lab_recognized, conf = mrecog.faceRecognition(findFace, labels, getStatusCam())
-    #if conf < 80:
-    r = peopcol.find_one({'p_id': lab_recognized})
-    print "Reconhecido o usuario ",r['p_name'], " com precisao de : ",100-conf, "%"
-    #else:
-    #    print "Pessoa nao identificada"
+    lab_recognized, conf = mrecog.faceRecognition(findFace)
+    if conf < 80:
+	r = peopcol.find_one({'p_id': lab_recognized})
+	print "Reconhecido o usuario ",r['p_name'], " com precisao de : ",100-conf, "%"
+    else:
+	print "Pessoa nao identificada"
 def saveandexit():
     mrecog.recognizer.save('save.xml')
-    pickle.dump(mrecog.imagens, open("imagens","wb"))
+    pickle.dump(mrecog.images, open("images","wb"))
 
--- mongRecog.py
+++ mongRecog.py
@@ -2,66 +2,45 @@
 
 #Pyctongo Face Detection
 
-import cv2, os, tkMessageBox, datetime, ttk
+import cv2, os, tkMessageBox, pymongo, datetime, ttk
 import numpy as np
 from PIL import Image
 
 faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-recognizer = cv2.createLBPHFaceRecognizer()
+recognizer = cv2.createFisherFaceRecognizer()
+images = []
+labels = []
 
-imagens = []
-
-def prepareImages(path_images, labels, number, statusCam):
-    if statusCam == False:
-        for imagem_atual in path_images:
-            imagem_pil = Image.open(imagem_atual).convert('L')
-            imagem = np.array(imagem_pil, 'uint8')
-            faces = faceCascade.detectMultiScale(imagem)
-            for (x, y, w, h) in faces:
-                imagens.append(imagem[y: y + h, x: x + w])
-                labels.append(number)
-        return len(path_images)
-    else:
-        imagem = np.array(path_images, 'uint8')
+def trainImages(path_images, labPer):
+    for imagem_atual in path_images:
+        imagem_pil = Image.open(imagem_atual).convert('L')
+        imagem = np.array(imagem_pil, 'uint8')
+        nbr = int(labPer)
         faces = faceCascade.detectMultiScale(imagem)
         for (x, y, w, h) in faces:
-            imagens.append(imagem[y: y + h, x: x + w])
-            labels.append(number)
-        return 1
+            images.append(imagem[y: y + h, x: x + w])
+            labels.append(nbr)
+        recognizer.train(images, np.array(labels))
 
-def updateTrain(path_images, numberLab, statusCam):
+def updateTrain(path_images, labPer):
     l_images=[]
     l_labels=[]
-    if statusCam == False:
-        for imagem_atual in path_images:
-            imagem_pil = Image.open(imagem_atual).convert('L')
-            imagem = np.array(imagem_pil, 'uint8')
-            faces = faceCascade.detectMultiScale(imagem)
-            for (x, y, w, h) in faces:
-                imagens.append(imagem[y: y + h, x: x + w])
-                l_images.append(imagem[y: y + h, x: x + w])
-                l_labels.append(numberLab)
-        recognizer.update(l_images, np.array(l_labels))
-        return len(path_images)
-    else:
-        imagem = np.array(path_images, 'uint8')
+    for imagem_atual in path_images:
+        imagem_pil = Image.open(imagem_atual).convert('L')
+        imagem = np.array(imagem_pil, 'uint8')
+        nbr = int(labPer)
         faces = faceCascade.detectMultiScale(imagem)
         for (x, y, w, h) in faces:
-            imagens.append(imagem[y: y + h, x: x + w])
+            images.append(imagem[y: y + h, x: x + w])
+            labels.append(nbr)
             l_images.append(imagem[y: y + h, x: x + w])
-            l_labels.append(numberLab)
+            l_labels.append(nbr)
         recognizer.update(l_images, np.array(l_labels))
-        return 1
 
-def faceRecognition(findFace, labels, statusCam):
-    recognizer.train(imagens, np.array(labels))
-    if statusCam == False:
-        predict_image_pil = Image.open(findFace).convert('L')
-    else:
-	predict_image_pil = findFace
+def faceRecognition(findFace):
+    predict_image_pil = Image.open(findFace).convert('L')
     predict_image = np.array(predict_image_pil, 'uint8')
     faces = faceCascade.detectMultiScale(predict_image)
     for (x, y, w, h) in faces:
-        nbr_predicted, conf = recognizer.predict(predict_image[y: y + h, x: x + w])
+        nbr_predicted, conf = recognizer.predict(predict_image[y: y + h, x: x + w])        
         return nbr_predicted, conf
-
